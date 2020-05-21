00d06af4e13cb1ba78e6856988068d4e58cd1a25
Neemias Abner <Neemias.Abner@edu.pucrs.br>
2020-05-21 15:24:28 -0300

    Classe para armazenar Dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 
 #include"IRPF.h"
-
+#include "Dados.h"
 
 int main(){
 
+    Dados teste;
+
+    teste.adicionar(IRPF("michelle guedes perez", "1234",100,200));
+    teste.adicionar(IRPF("neemias abner", "1235",100,200));
+
+
+    cout << teste.ler("1232").Getnome();
 
 }
