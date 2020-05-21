050c1c5b2df4ecc63a608f84e2700cfed68049c8
Neemias Abner <Neemias.Abner@edu.pucrs.br>
2020-05-21 17:34:49 -0300

    Correção de CPF duplicado

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@ int main(){
     teste.adicionar(IRPF("michelle guedes perez", "1234",100,200));
     teste.adicionar(IRPF("neemias abner", "1235",100,2000));
 
-
     cout << teste.ler("1235").Getnome() << endl << teste.ler("1235").imposto_completo() ;
 
 }
--- src/Dados.cpp
+++ src/Dados.cpp
@@ -10,9 +10,13 @@ Dados::Dados()
 
 
  int Dados::adicionar(IRPF etodo){
-
+    for (int i=0;i<todos.size();i++){
+            if(todos.at(i).Getcpf()==etodo.Getcpf()){
+                todos[i]=etodo;
+                return 0;}
+    }
     todos.push_back(etodo);
-
+    return 1;
 }
 
 IRPF Dados::ler(string ecpf){
