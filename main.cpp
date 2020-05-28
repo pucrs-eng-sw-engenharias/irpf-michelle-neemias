31157ce044bbfacb1a519e9f552dac3a25165790
Michelle Perez <guedes.perez@gmail.com>
2020-05-28 16:48:58 -0300

    Finalizando Interface

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 
-#include"IRPF.h"
+#include "IRPF.h"
 #include "Dados.h"
+#include "Interface.h"
+
+
+
 
 int main(){
-
     Dados teste;
+    teste.adicionar(IRPF("Michelle Guedes Perez", "1235",2000,20000));
+    teste.adicionar(IRPF("Neemias Abner dos Reis Pereira", "1234",1000,15000));
 
-    teste.adicionar(IRPF("michelle guedes perez", "1234",100,200));
-    teste.adicionar(IRPF("neemias abner", "1235",100,2000));
-
-    cout << teste.ler("1235").Getnome() << endl << teste.ler("1235").imposto_completo() ;
+    menu(&teste);
 
 }
